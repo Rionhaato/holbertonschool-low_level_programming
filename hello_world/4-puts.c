0bee0a6de91d8b05dfa6c47b5cd1f5c750a56c56
root <root@ip-10-42-159-183.ec2.internal>
2025-06-13 00:31:01 +0000

    4-puts.c: print message with puts and return 0

---
+++ hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints a message with puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- hello_world/README.md
+++ hello_world/README.md
@@ -2,3 +2,4 @@
 
 Runs the C preprocessor on the file in $CFILE and saves the output to file c.
 - **3-name**: compiles `$CFILE` into an executable named `cisfun` using `gcc`.
+- **4-puts.c**: prints a fixed string using `puts`, then returns 0.
