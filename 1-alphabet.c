b90baade144f4d4ad5568af69c5d64031125e35d
root <root@ip-10-42-112-135.ec2.internal>
2025-06-15 16:16:33 +0000

    Fix print_alphabet function - Betty style compliance

--- 1-alphabet.c
+++ 1-alphabet.c
@@ -1,18 +1,13 @@
-#include "../main.h"
+#include "main.h"
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line
- *
- * Return: Always 0.
  */
 void print_alphabet(void)
 {
-    char letter = 'a';
-    
-    while (letter <= 'z')
-    {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
