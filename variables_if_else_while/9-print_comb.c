f729512e15a5b600e0a2cffc9affca11a3ac411b
root <root@ip-10-42-12-36.ec2.internal>
2025-06-15 00:58:45 +0000

    Add 9-print_comb.c print all combinations of single digit numbers

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i, j;
+
+i = 0;
+while(i <= 8)
+{
+j = i + 1;
+while (j <=9)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (!(i == 8 && j == 9))
+{
+putchar(',');
+putchar(' ');
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
