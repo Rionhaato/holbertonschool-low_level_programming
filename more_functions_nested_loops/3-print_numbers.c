3ec16e8f1f358a9d9c28c841eb8c7615704d0480
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-10-20 21:31:24 -0400

    Fix Betty documentation style for print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print numbers - prints the numbers 0 through 9, followed by a new line
+ * print_numbers - prints the numbers from 0 to 9, followed by a new line
  *
  * Return: void
  */
