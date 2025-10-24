a92d8572edf3e576d288b5b8f20d307e8283a50d
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-10-24 18:28:50 -0400

    Task 9: Implement _strcpy to copy a string including null terminator

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: buffer where we copy the string
+ * @src: the source string to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+
+	while (1)
+	{
+		dest[1] = src[i];
+		if (src[i] == '\0')
+			break;
+		i++;
+	}
+	return (dest);
+}
