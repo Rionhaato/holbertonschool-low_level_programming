3a7f829e8a724f92abdc5a8e288efe261e61ad02
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-10-29 16:43:47 -0400

    recursion: implement _pow_recursion (task 4) and add minimal test main

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - returns x raised to the power of y
+ * @x: base integer
+ * @y: exponent integer
+ *
+ * Return: x^y; -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
