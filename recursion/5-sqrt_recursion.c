2c38cdadf47be656286df286e609fcda95a56c06
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-10-30 13:31:40 -0400

    style(recursion): Betty fixes (LF endings, trim, wrap, tabs)

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,7 +4,7 @@
  * sqrt_check - Helper function to check for the square root recursively
  * @n: The number to find the square root of
  * @guess: The current guess for the square root
- * 
+ *
  * Return: natural square root of n if found, otherwise -1
  */
 int sqrt_check(int n, int guess)
@@ -21,14 +21,15 @@ int sqrt_check(int n, int guess)
 /**
  * _sqrt_recursion - Returns the natural square root of a number
  * @n: number to compute the square root for
- * 
- * Return: The natural square root of n, or -1 if n does not have a natural square root
- */	 
+ *
+ * Return: The natural square root of n,
+ * or -1 if n does not have a natural square root
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0)
-        return (0);
-    return (sqrt_check(n, 1));
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	return (sqrt_check(n, 1));
 }
