e35978cffc0dabe0e911fa79854512c7c3f0e463
Joshua Santiago <joshua.d.santiago92@gmail.com>
2025-12-03 09:43:13 -0400

    Add hash_table_print for table output

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+/*
+ * File: 5-hash_table_print.c
+ * Author: Joshua
+ */
+
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: Hash table to print.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
